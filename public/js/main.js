d3eccee7a3dc6ef585ddd336f7f54b56ca761f31
Chris Stenke (Isfett) <chris@isfett.com>
2017-10-08 16:38:55 +0200

    Promise all for the win

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,10 @@
 import {makeRequest} from "./lib/http.js";
-import {delaySeconds} from "./lib/wait.js";
 
-makeRequest('GET','/config/config.json', 3000)
-    .then(response => {
-        return JSON.parse(response);
-    })
-    .then(json => {
-        console.log(json);
-        return true;
-    })
-    .then(() => {
-        return makeRequest('GET', '/index.html', 5000);
-    })
-    .then(response => {
-        console.log('response', response);
-        alert('loaded');
-    })
-    .catch((error) => {
-        console.log('Error: ', error);
-    });
+Promise.all([
+    makeRequest('GET', '/config/config.json', 3000),
+    makeRequest('GET', '/index.html', 5000),
+])
+.then(([configResponse, indexResponse]) => {
+    console.log(JSON.parse(configResponse));
+    console.log('indexResponse', indexResponse);
+});
